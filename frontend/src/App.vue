57a8b61e860679e1de01b67aa801162414302b13
vasilisa <vasilisa_vasileva_2000@inbox.ru>
2023-12-22 03:28:22 +0300

    Final ver

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main-container">
+    <h1>Delivery Autofill</h1>
+    <Autocomplete />
+  </div>
+</template>
+
+<script>
+import Autocomplete from './components/Autocomplete.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Autocomplete
+  }
+}
+</script>
+
+<style>
+
+  .main-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    font-family: 'Fredoka', sans-serif;
+  }
+
+  h1 {
+    font-size: 3rem;
+  }
+
+  @import url('https://fonts.googleapis.com/css2?family=Fredoka&display=swap');
+</style>
